c0a6c39746649dd772258686e6292680ed2b3ef8
Anand kumar <119012966+anandkumarpatel1@users.noreply.github.com>
2023-07-29 16:14:05 +0530

    .

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -12,7 +12,7 @@ const {
 
 const router = express.Router();
 
-router.route("/order/new").post(isAuthenticatedUser, newOrder);
+router.route("/order/new").post(newOrder);
 
 router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
 
