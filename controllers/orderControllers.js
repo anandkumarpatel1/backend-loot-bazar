24c8e33fda8a3a8f16d6b542bcca0f47f7817da3
Anand kumar <119012966+anandkumarpatel1@users.noreply.github.com>
2023-07-29 16:32:59 +0530

    .

--- controllers/orderControllers.js
+++ controllers/orderControllers.js
@@ -23,7 +23,8 @@ exports.newOrder = catchAsyncErrors(async (req, res, next) => {
     taxPrice,
     shippingPrice,
     totalPrice,
-    paidAt: Date.now()
+    paidAt: Date.now(),
+    // user: req.user._id,
   });
 
   res.status(201).json({
