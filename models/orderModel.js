e9c02913f32a807685e9d47427c355352dddcc49
Anand kumar <119012966+anandkumarpatel1@users.noreply.github.com>
2023-07-29 16:22:09 +0530

    .

--- models/orderModel.js
+++ models/orderModel.js
@@ -56,7 +56,7 @@ const orderSchema = new mongoose.Schema({
     type: mongoose.Schema.ObjectId,
     ref: "User",
     required: true,
-    default: 0,
+    default: "user",
   },
   paymentInfo: {
     id: {
